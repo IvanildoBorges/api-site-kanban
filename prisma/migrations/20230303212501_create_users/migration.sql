15fb2de53f762daf4f861b0eb223a4ed40772177
IvanildoBorges <ivanildo.rap@gmail.com>
2023-03-03 19:46:29 -0300

    Alterado nome para name na Prisma migrate

--- prisma/migrations/20230303212501_create_users/migration.sql
+++ prisma/migrations/20230303212501_create_users/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "nome" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
